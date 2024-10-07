cb90d148bfc215cb436307a64f94aa24077e9521
BRVallmi <118301519+BRVallmi@users.noreply.github.com>
2024-10-07 20:05:56 +0200

    Base proyecto

---
+++ AngualarApiPaises/src/app/info-paises/info-paises.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-paises',
+  templateUrl: './info-paises.component.html',
+  styleUrl: './info-paises.component.css'
+})
+export class InfoPaisesComponent {
+  
+}
